20deee73e28d53247d4a213304ecafb9fab59d26
khanh <devkhanhle1102@gmail.com>
2022-03-26 09:49:55 +0700

    fix fe

--- Dormitory.Admin.Web/src/app/main/main.module.ts
+++ Dormitory.Admin.Web/src/app/main/main.module.ts
@@ -7,16 +7,22 @@ import { LayoutComponent } from './layout/layout.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzListModule } from 'ng-zorro-antd/list';
-
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { AreaComponent } from './area/area.component';
+import { RoomComponent } from './room/room.component';
+import { FacilityComponent } from './facility/facility.component';
+import { CriteriaComponent } from './criteria/criteria.component';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    AreaComponent,
+    RoomComponent,
+    FacilityComponent,
+    CriteriaComponent,
   ],
   imports: [
     CommonModule,
